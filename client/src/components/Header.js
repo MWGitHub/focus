0bfd33a85cac8e3a58f5284cb71b564346810452
Michael Wang <mwwpublic@gmail.com>
2015-09-02 20:12:08 -0400

    Add style to the nav menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -21,9 +21,8 @@ class AuthNav extends React.Component {
     }
 
     render() {
-        var visibilityFlag = this.state.isMenuVisible ? 'visible' : 'hidden';
         var menu = (
-            <ul className={"nav-menu-content " + visibilityFlag}>
+            <ul className="nav-menu-content">
                 <li><Router.Link to={Routes.board}>Board</Router.Link></li>
                 <li><Router.Link to={Routes.logout}>Log Out</Router.Link></li>
             </ul>
@@ -32,7 +31,7 @@ class AuthNav extends React.Component {
         return (
             <nav className="nav-menu">
                 <a className="button" onClick={this._toggleMenu.bind(this)}>{this.props.username}</a>
-                {menu}
+                {this.state.isMenuVisible ? menu : null}
             </nav>
         )
     }
