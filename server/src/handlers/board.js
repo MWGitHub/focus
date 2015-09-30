ee2c7cb459cb484485d9ead9f18aa5f50c8d0d0e
Michael Wang <mwwpublic@gmail.com>
2015-09-30 11:45:20 -0400

    Complete conversion from promises to co for board

--- server/src/handlers/board.js
+++ server/src/handlers/board.js
@@ -48,24 +48,18 @@ var handler = {
         "use strict";
 
         var boardID = request.params['id'];
-        var userId;
-        User.forge({id: request.auth.credentials.id}).fetch({require: true})
-            .then(function (user) {
-                userId = user.get('id');
-                return Board.forge({id: boardID}).fetch({require: true});
-            })
-            .then(function(board) {
-                if (board.get('user_id') !== userId) {
-                    throw Boom.unauthorized();
-                }
-                return board.retrieveAsData(true);
-            })
-            .then(function(data) {
-                reply(API.makeData(data));
-            })
-            .catch(function(err) {
-                reply(Boom.wrap(err));
-            });
+        return co(function* () {
+            var user = yield User.forge({id: request.auth.credentials.id}).fetch({require: true});
+            var userId = user.get('id');
+            var board = yield Board.forge({id: boardID}).fetch({require: true});
+            if (board.get('user_id') !== userId) {
+                throw Boom.unauthorized();
+            }
+            var data = yield board.retrieveAsData(true);
+            reply(API.makeData(data));
+        }).catch(function(error) {
+            reply(Boom.wrap(error));
+        });
     }
 };
 
